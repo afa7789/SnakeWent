5cae81ac0239c2ca8bd7ff50bf6d76214752ba1c
arthur.abeilice <arthur.abeilice@codificar.com.br>
2021-09-28 18:29:42 -0300

    game logics 101, more details for future programmiing

--- pkg/gamelogic/gamelogic.go
+++ pkg/gamelogic/gamelogic.go
@@ -130,15 +130,35 @@ func moveSnake(){
 }
 
 func hasEaten() bool{
+	// check if next position is one of the foods position, if it is increase snake size 
+	// and add a node to it's last node.
+	// change last node to new last node.
 	return false
 }
 
 func addFood() bool{
+	// add a food to the nodeList of foods.
 	return false
 }
 
+func headIsReturning() bool{
+	// check next node from head position , if it's the same from next, if it is can't allow it for next pos.
+	return false
+}
+
+func checkSelfHit() bool{
+	// check if snaker hit itself, if it does it has to lose.
+	return false
+}
+
+// check if next position hit's border
 func checkBorderHit(i ,j,width,height int) bool{
 	return false
+}
+
+func roundEnding(){
+	// increase score for one more round, or print game end state.
+	// not sure what else
 }
 
 func boardTesting(){
