3af88c6ca9aa09c4a40d6631cc6501f1ffc476da
arthur.abeilice <arthur.abeilice@codificar.com.br>
2021-09-28 11:29:05 -0300

    restructuring, tests and gitignore

--- pkg/printer/printer.go
+++ pkg/printer/printer.go
@@ -9,7 +9,24 @@ func HelloWorld() {
 	fmt.Println("Hello World")
 }
 
+//Testing function used to learn more, this was changed while developing
 func TestAscii() {
-	ascBlock := string(219)
-	fmt.Println(ascBlock)
+
+	const size int = 7
+	charArray := [size]int{'█','╔','╚','╗','╝','═','║'} 
+
+	for d := 0; d < size; d++ {
+		fmt.Printf("%v - ",charArray[d])
+		fmt.Println(string(charArray[d]))
+	}
+
+	/*	
+		9608 - █
+		9556 - ╔
+		9562 - ╚
+		9559 - ╗
+		9565 - ╝
+		9552 - ═
+		9553 - ║
+	*/
 }
