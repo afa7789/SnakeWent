4e6776e10aa01f5c71b02946fe763bab6d58cbbd
arthur.abeilice <arthur.abeilice@codificar.com.br>
2021-09-27 21:56:28 -0300

    snake went, initial commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"./pkg/printer"
+)
+
+func main() {
+	printer.HelloWorld()
+}
---
+++ pkg/printer/printer.go
@@ -0,0 +1,15 @@
+package printer
+
+import (
+	"fmt"
+)
+
+/**
+*/
+func HelloWorld() {
+	fmt.Println("Hello World")
+}
+
+func HelloWorld() {
+	fmt.Println("Hello World")
+}
