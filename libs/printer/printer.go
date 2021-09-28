d8b79bb96683b221a22351ce7d8ecd1b073dbcc1
arthur.abeilice <arthur.abeilice@codificar.com.br>
2021-09-27 22:40:29 -0300

    aaa

---
+++ libs/printer/printer.go
@@ -0,0 +1,14 @@
+package printer
+
+import (
+	"fmt"
+)
+
+// Hello World, é uma função de Teste para testar blibioteca.
+func HelloWorld() {
+	fmt.Println("Hello World")
+}
+
+func PrintOther() {
+	fmt.Println("Hello World")
+}
