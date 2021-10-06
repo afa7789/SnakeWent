02950913894915378d6c30785e3bacb275c08a0c
arthur.abeilice <arthur.abeilice@codificar.com.br>
2021-10-06 14:08:14 -0300

    documentation

--- pkg/game/boardlogic.go
+++ pkg/game/boardlogic.go
@@ -2,24 +2,17 @@ package game
 
 // fillBorders changes int from board state to empty board
 func fillBorders(sample *[][]int, width, height int) {
-	// fill borders ,
-	// for  i := range *sample {
-	// 	for j, val := range (*sample)[i] {
-	// 		if j == 0 || j == width+1 || i == 0 || i == height + 1 {
-	// 			(*sample)[i][j] =
-	// 		}
-	// 	}
-	// }
+	// vertical bars
 	for i := 1; i <= height; i++ {
 		(*sample)[i][0] = cornerVertical
 		(*sample)[i][height+1] = cornerVertical
 	}
-
+	//horizontal bars
 	for i := 1; i <= width; i++ {
 		(*sample)[0][i] = cornerHorizontal
 		(*sample)[width+1][i] = cornerHorizontal
 	}
-
+	//corners
 	(*sample)[0][0] = cornerUpLeft
 	(*sample)[0][width+1] = cornerUpRight
 	(*sample)[height+1][0] = cornerDownLeft
@@ -38,26 +31,26 @@ func fillEmptyDots(sample *[][]int, height, width int) {
 }
 
 // fillWithCharacterFromNode, reutilization of code
-func fillWithCharacterFromNode(sample *[][]int, i int, nl nodeList) {
+func fillWithCharacterFromNode(sample *[][]int, nl nodeList, i int) {
 	//genericFunction to be used on fillFood and fillSnake
-}
-
-// fillFood, Change int on board for foods and check if adds more.
-func fillFood(sample *[][]int, nl nodeList) {
-	// check round number to see if can add one more food
-
-	// receive a node with foods
-	// while node != nil check it's position to the food one
-	// change node pointer to next one.
-}
-
-// fillSnake, Change int on board for snake int
-func fillSnake(sample *[][]int, nl nodeList) {
 	// while node != nil
 	// receive node , check it's position and change the gameboard Character to the snake one.
 	// change node pointer to next node
 }
 
+// fillFood, Change int on board for foods and check if adds more.
+func fillFood(sample *[][]int, nl nodeList) {
+	// call the function and fills with diamondEmpty int
+	fillWithCharacterFromNode(sample, nl, diamondEmpty)
+}
+
+// fillSnake, Change int on board for snake int
+func fillSnake(sample *[][]int, nl nodeList) {
+	// call the function and fills with rectangle int
+	fillWithCharacterFromNode(sample, nl, rectangle)
+}
+
+// Call all fills, that will be called more than one time.
 func fillAll(g gameState) {
 	fillEmptyDots(g.board, g.height, g.width)
 	fillFood(g.board, g.foodList)
